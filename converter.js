0f0fe368a155af9c5ed6979212c38172eb5be6ab
Muhammad-Adnan-s <madnan7686110@gmail.com>
2024-09-17 08:55:59 +0500

    Update converter.js

--- converter.js
+++ converter.js
@@ -3,16 +3,9 @@ let btn = document.querySelector('.button');
 let underline = document.querySelector('.underline');
 let label = document.querySelector('.label');
 let result = document.querySelector('.result');
-let oneheading = document.querySelector('.one');
-let twoheading = document.querySelector('.two');let input = document.querySelector('.CGPA');
-let btn = document.querySelector('.button');
-let underline = document.querySelector('.underline');
-let label = document.querySelector('.label');
-let result = document.querySelector('.result');
 let result1 = document.querySelector('.result1');
 let oneheading = document.querySelector('.one');
 let twoheading = document.querySelector('.two');
-
 
 oneheading.addEventListener('click',()=>{
     underline.classList.remove('translate');
@@ -66,38 +59,4 @@ function gpa(){
 
 function clearinput(){
     input.value='';
-}
-
-
-twoheading.addEventListener('click', () => {
-    underline.classList.add('translate');
-    input.placeholder = 'Enter the GPA';
-    label.innerText = 'GPA';
-    btn.addEventListener('click', cgp)
-})
-oneheading.addEventListener('click', () => {
-    underline.classList.remove('translate');
-    input.placeholder = 'Enter the CGPA';
-    label.innerText = 'CGPA';
-    btn.addEventListener('click', gpa)
-})
-function clear() {
-    input.value = '';
-}
-// finding gpa
-btn.addEventListener('click', gpa);
-
-
-function cgp() {
-    let num = Number(input.value);
-    let cgpa = num * 2.5;
-    result.innerText = cgpa;
-    clear();
-}
-// gpa
-function gpa() {
-    let value = Number(input.value);
-    let gp = (value * 4) / 10;
-    result.innerText = gp;
-    clear();
 }
